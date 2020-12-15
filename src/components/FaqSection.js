5a423ae67f25787d6e97bbe9f89b7650fb9c51b9
Gamilgton <gamilgton@users.noreply.github.com>
2020-12-15 20:30:58 +0100

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h3>Any questions <span>FAQ</span></h3>
+
+            <div className="question">
+                <h4>How do I start?</h4>
+
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consequuntur, dolor libero excepturi sapiente porro beatae vero ratione soluta nobis numquam recusandae aspernatur tempora corrupti, saepe doloremque quia? Delectus, neque!</p>
+                </div>
+
+            </div>
+
+            <div className="question">
+                <h4>Daily Schedule</h4>
+
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consequuntur, dolor libero excepturi sapiente porro beatae vero ratione soluta nobis numquam recusandae aspernatur tempora corrupti, saepe doloremque quia? Delectus, neque!</p>
+                </div>
+
+            </div>
+
+            <div className="question">
+                <h4>Different payment methods</h4>
+
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consequuntur, dolor libero excepturi sapiente porro beatae vero ratione soluta nobis numquam recusandae aspernatur tempora corrupti, saepe doloremque quia? Delectus, neque!</p>
+                </div>
+
+            </div>
+
+            <div className="question">
+                <h4>What products do you offer?</h4>
+
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consequuntur, dolor libero excepturi sapiente porro beatae vero ratione soluta nobis numquam recusandae aspernatur tempora corrupti, saepe doloremque quia? Delectus, neque!</p>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default FaqSection;
