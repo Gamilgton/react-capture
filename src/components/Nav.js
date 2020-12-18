10cf0741add565adade84b3bac38cba92d089ae8
Gamilgton <gamilgton@users.noreply.github.com>
2020-12-18 12:00:50 +0100

    9. React Router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,26 +4,29 @@ import React from 'react';
 import {Navigation} from '../styles';
 // import styled from 'styled-components';
 
+// Router
+import {Link} from 'react-router-dom';
+
 const Nav = () => {
     return (
 
         <Navigation>
 
             <h1>
-                <a id="logo" href="#">Capture</a>
+                <Link id="logo" to="/">Capture</Link>
             </h1>
 
             <ul>
                 <li>
-                    <a href="#">1. Abut Us</a>
+                    <Link to="/">1. Abut Us</Link>
                 </li>
 
                 <li>
-                    <a href="#">2. Our work</a>
+                    <Link to="/work">2. Our work</Link>
                 </li>
 
                 <li>
-                    <a href="#">3. Contact Us</a>
+                    <Link to="/contact">3. Contact Us</Link>
                 </li>
             </ul>
 
